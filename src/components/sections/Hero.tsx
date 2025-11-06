d6c484517ae8b2cce7d18f6f5ea45bbff3b42d08
Maksym <mtovkay@gmail.com>
2025-11-06 11:41:14 +0100

    add video

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -11,15 +11,8 @@ export default function Hero() {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
     >
-      <Image src="/logo_bg.png" alt="Hero background" fill className="object-cover" />
-      <MotionDiv
-        className="absolute top-0 left-0 w-full h-full flex items-center justify-center"
-        initial={{ scale: 0.8, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 1, delay: 0.3, ease: "easeOut" }}
-      >
-        <Image src="/directgold_logo.png" alt="Logo" width={450} height={450} className="object-contain max-sm:w-[300px] max-sm:h-[300px]" />
-      </MotionDiv>
+      
+      <video src="/Intro Animation.mp4" autoPlay muted className="absolute inset-0 w-full h-full object-cover" />
     </MotionSection>
   );
 }
